a0d5dde09a5dc58afbec78817efea7bebd179bd8
Yosuke Yasuda <double.y.919.quick@gmail.com>
2015-10-25 10:44:16 -0400

    change static

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('', express.static('www'));
 app.use(expressValidator());
 
 app.use('/', routes);
